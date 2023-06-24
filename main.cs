1a0966df099720bf1f42fb3379be65e792f33f2d
Ivan Zakharov <Ivan.Zakharov.aob@gmail.com>
2023-06-24 05:22:02 +0300

    Style fixes

--- main.cs
+++ main.cs
@@ -20,43 +20,29 @@ internal sealed class Main : MelonMod
 			MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization");
 			Localization.s_WorkshopStringTable = ScriptableObject.CreateInstance<StringTable>();
 			Localization.s_WorkshopStringTable.SetGeneratedByCode();
-			string text = "";
-			DirectoryInfo directoryInfo = Directory.GetParent(Application.dataPath);
-			if (directoryInfo != null)
-			{
-				directoryInfo = directoryInfo.Parent;
-			}
-			if (directoryInfo != null)
-			{
-				directoryInfo = directoryInfo.Parent;
-			}
-			if (directoryInfo != null)
-			{
-				text = directoryInfo.FullName + "/workshop/content/305620";
-			}
+			
+			var directoryInfo = Directory.GetParent(Application.dataPath);
+			var text = directoryInfo?.Parent?.Parent?.FullName + "/workshop/content/305620";
 			MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization " + text);
-			if (Directory.Exists(text))
-			{
-				MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization exists");
-				string[] files = Directory.GetFiles(text, "*.csv", SearchOption.AllDirectories);
-				for (int i = 0; i < files.Length; i++)
-				{
-					MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization " + files[i]);
-					Il2CppSystem.Collections.Generic.List<string> entries = Localization.s_WorkshopStringTable.ImportCsv(files[i], null);
-					MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization " + files[i] + " | " + entries.Count);
-				}
-				foreach (string text2 in Localization.s_WorkshopStringTable.GetLanguages())
-				{
-					MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization each " + text2);
-					if (!Localization.s_Languages.Contains(text2))
-					{
-						MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization add " + text2);
-						Localization.s_Languages.Add(text2);
-					}
-				}
-			}
 
+			if (!Directory.Exists(text)) return;
+			MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization exists");
+			
+			var files = Directory.GetFiles(text, "*.csv", SearchOption.AllDirectories);
+			foreach (var file in files)
+			{
+				MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization " + file);
+				Il2CppSystem.Collections.Generic.List<string> entries = Localization.s_WorkshopStringTable.ImportCsv(file, null);
+				MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization " + file + " | " + entries.Count);
+			}
+			
+			foreach (var text2 in Localization.s_WorkshopStringTable.GetLanguages())
+			{
+				MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization each " + text2);
+				if (Localization.s_Languages.Contains(text2)) continue;
+				MelonLoader.MelonLogger.Warning("LoadWorkshopLocalization add " + text2);
+				Localization.s_Languages.Add(text2);
+			}
 		}
 	}
-
 }
